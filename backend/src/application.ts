611a2c6bb0b9647663c868a3f928d796d0cefc77
Marie-Anne Grand <grand_m@epita.fr>
2019-12-05 18:15:19 +0100

    Add connexion to DB

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,8 +1,33 @@
 import * as express from "express";
+import {Response} from "express";
+import * as mysql from "mysql";
+import "reflect-metadata";
+import {dbConfig} from "../apiConfig";
 import {pingController} from "./controllers/ping";
 
 const app = express();
 
 app.use(pingController);
+
+const connexion = mysql.createConnection(dbConfig);
+
+connexion.connect((err: Error) => {
+    if(!err) {
+        console.log("Database is connected ... nn");
+    } else {
+        console.log("Error connecting database ... nn");
+    }
+});
+
+app.get("/",(res: Response) => {
+    connexion.query('SELECT * FROM events LIMIT 5', (err, rows) => {
+        if (!err) {
+            res.json(rows);
+        }
+        else {
+            console.log('Error while performing Query. ' + err);
+        }
+    });
+});
 
 export default app;
