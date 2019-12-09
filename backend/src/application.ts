e8d54167e721585fcd279a1d5474a92425bf2d37
Marie-Anne Grand <grand_m@epita.fr>
2019-12-09 19:17:27 +0100

    Remove useless api call

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -2,14 +2,12 @@ import * as express from "express";
 import "reflect-metadata";
 import {database} from "./controllers/databaseConnexion";
 import {eventsController} from "./controllers/events";
-import {pingController} from "./controllers/ping";
 
 const app = express();
 app.use((_, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     next();
 });
-app.use(pingController);
 database.connect((err: Error) => {
     if(!err) {
         app.use('/api/events', eventsController);
